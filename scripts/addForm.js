ff7006132dafe2dfc771406aa8043da9f771fdd0
Brett Hughes <twofivinclimber@gmail.com>
2022-04-29 17:19:35 -0500

    ID function added, clear form added, new style added.

--- scripts/addForm.js
+++ scripts/addForm.js
@@ -3,9 +3,10 @@ import { filterFunction } from "./filerFunction.js";
 
 //Add Animal
 const addForm = () => {
-  document.querySelector("#form").addEventListener("submit", (e) => {
+  document.querySelector("#petForm").addEventListener("submit", (e) => {
     e.preventDefault();
     const newAnimalObj = {
+      id: pets[pets.length - 1].id + 1,
       name: document.querySelector("#petName").value,
       color: document.querySelector("#petColor").value,
       specialSkill: document.querySelector("#petSkill").value,
@@ -14,7 +15,9 @@ const addForm = () => {
     };
     pets.push(newAnimalObj);
     filterFunction(pets);
-    form.reset("#form");
+    document.getElementById("petForm").reset();
+    //Check to see id is being generated.
+    console.log(pets[pets.length - 1].id);
   });
 };
 
